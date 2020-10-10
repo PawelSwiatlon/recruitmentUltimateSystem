5f81ae80c72ac0ce7396f5bc5073616271357595
PawelSwiatlon <pswiatlon02@gmail.com>
2020-10-10 13:35:58 +0200

    Add search worker metchod

--- assets/scripts/worker.js
+++ assets/scripts/worker.js
@@ -1,8 +1,9 @@
-let workers = [
+const workersData = [
     {name: "Adam Nowak", img: "worker1.jpg", chosen: false},
     {name: "Michał Potoczek", img: "worker2.jpg", chosen: false},
     {name: "Antoni Worek", img: "worker3.jpg", chosen: false}
 ];
+let workers = workersData;
 let workerList = document.getElementById("options");
 function createWorkerList() {
     workerList.innerHTML = "";
@@ -31,20 +32,31 @@ function showlist() {
     let test = document.getElementById("option-container");
     test.style.maxHeight = "300px";
     test.style.borderWidth = "1px";
-
 }
 function hiddenlist() {
     let test = document.getElementById("option-container");
     test.style.maxHeight = "0px";
     test.style.borderWidth = "0px";
-
 }
 function chosen(id) {
     document.getElementById("pracownik").value = workers[id].name;
-    // document.getElementById("pracownik").setAttribute("value", workers[id].name);
     for(let i in workers){
         workers[i].chosen = false
     }
     workers[id].chosen = true;
+    document.getElementById("szukaj").value = '';
+    workers = workersData;
+    createWorkerList();
+}
+function filterWorkerList() {
+    let user = document.getElementById("szukaj").value;
+    if(user.length == 0) {
+        workers = workersData;
+    } else {
+        workers = workersData;
+        workers = workers.filter(worker => {
+            return worker.name.toLowerCase().indexOf(user.toLowerCase()) !== -1
+        });
+    }
     createWorkerList();
 }
